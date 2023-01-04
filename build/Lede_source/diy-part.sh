4ef845252b9738bfca6ffd0b5b64590b0785857e
lq-wq <61339326+lq-wq@users.noreply.github.com>
2023-01-04 10:30:17 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -31,7 +31,7 @@ uci set system.@system[0].hostname='OpneWRT-KH'              # 修改主机名�
 EOF
 
 #添加插件
-echo 'src-git Packages https://github.com/lq-wq/Packages' >>feeds.conf.default
+echo 'src-git Packages https://github.com/lq-wq/Packages.git' >>feeds.conf.default
 
 # 把bootstrap替换成argon为源码必选主题（可自行修改您要的,主题名称必须对,比如下面代码的[argon],源码内必须有该主题,要不然编译失败）
 sed -i "s/bootstrap/argon/ig" feeds/luci/collections/luci/Makefile
